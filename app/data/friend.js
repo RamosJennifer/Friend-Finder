290bc3a6a582e835d52de33597463396b569beb0
RamosJennifer <jenniferramos4858@gmail.com>
2019-11-20 23:47:23 -0600

    created an array of friends inside of friend.js

---
+++ app/data/friend.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Jared Ramos",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Josue Ramos",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    }
+]
